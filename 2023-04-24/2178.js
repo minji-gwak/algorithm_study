20731ec7886c9093ca10dda8dd24646977aa1d48
곽민지 <mini0006@naver.com>
2023-05-14 12:26:58 +0900

    미로 탐색

--- 2023-04-24/2178.js
+++ 2023-04-24/2178.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const [L, ...datas] = fs.readFileSync(filePath).toString().trim().split('\n');
 
-function solution([yMax, xMax], map) {
+function solution([yMin, xMin], map) {
   const queue = [[0, 0, 1]];
 
   const directions = [
@@ -19,16 +19,14 @@ function solution([yMax, xMax], map) {
       const xPos = x + directions[i][0];
       const yPos = y + directions[i][1];
 
-      if (0 <= xPos && yPos > -1 && xPos < xMax && yPos < yMax) {
-        if (map[yPos][xPos] === 1) {
-          map[yPos][xPos] = distance + 1;
-          queue.push([xPos, yPos, distance + 1]);
-        }
+      if (0 <= xPos && yPos > -1 && xPos < xMin && yPos < yMin && map[yPos][xPos] === 1) {
+        map[yPos][xPos] = distance + 1;
+        queue.push([xPos, yPos, distance + 1]);
       }
     }
   }
 
-  console.log(map[yMax - 1][xMax - 1]);
+  console.log(map[yMin - 1][xMin - 1]);
 }
 
 solution(
