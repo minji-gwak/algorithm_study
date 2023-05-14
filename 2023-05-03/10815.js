1411b91f6728bb9a65935f0380cbb2ede1b817ea
곽민지 <mini0006@naver.com>
2023-05-14 22:00:24 +0900

    숫자 카드

--- 2023-05-03/10815.js
+++ 2023-05-03/10815.js
@@ -2,14 +2,27 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const [N, nList, M, mList] = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const solution = (cardList, checkList) => {
-  const haveCardSet = new Set(cardList);
+const solution = (haveCnt, cardList, checkList) => {
+  cardList.sort((a, b) => a - b);
   const answer = Array();
 
   for (checkNum of checkList) {
-    answer.push(haveCardSet.has(checkNum) ? 1 : 0);
+    let [start, end] = [0, haveCnt - 1];
+    let isHave = false;
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      if (cardList[mid] > checkNum) {
+        end = mid - 1;
+      } else if (cardList[mid] < checkNum) {
+        start = mid + 1;
+      } else {
+        isHave = true;
+        break;
+      }
+    }
+    answer.push(isHave ? 1 : 0);
   }
   console.log(answer.join(' '));
 };
 
-solution(nList.split(' ').map(Number), mList.split(' ').map(Number));
+solution(Number(N), nList.split(' ').map(Number), mList.split(' ').map(Number));
