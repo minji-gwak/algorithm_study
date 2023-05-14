485ee784b184c62d8bf1be59bcc19721f3e43c8d
곽민지 <mini0006@naver.com>
2023-05-14 20:03:19 +0900

    수들의 합

---
+++ 2023-05-01/1789.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const S = Number(fs.readFileSync(filePath).toString().trim());
+
+function solution(sum) {
+  let start = 1;
+  let end = sum === 1 ? 1 : sum - 1;
+  let maxCnt = Number.MIN_SAFE_INTEGER;
+
+  while (start <= end) {
+    let mid = ~~((start + end) / 2);
+    let seqSum = (mid * (1 + mid)) / 2;
+    if (seqSum > sum) {
+      end = mid - 1;
+    } else {
+      if (maxCnt <= mid) {
+        maxCnt = mid;
+      }
+      start = mid + 1;
+    }
+  }
+
+  console.log(maxCnt);
+}
+
+solution(Number(S));
