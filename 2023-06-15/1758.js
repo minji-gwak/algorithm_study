aebff698cb3edfe0b9b70334cf23cca0e8fee0a3
곽민지 <mini0006@naver.com>
2023-06-17 01:46:20 +0900

    알바생 강호

--- 2023-06-15/1758.js
+++ 2023-06-15/1758.js
@@ -6,7 +6,7 @@ const solution = (N, tipList) => {
   tipList.sort((a, b) => b - a);
   let sum = 0;
 
-  for (let i = N - 1; i >= 0; i--) {
+  for (let i = 0; i < N; i++) {
     let tips = tipList[i] - i;
     if (tips > 0) {
       sum += tips;
