2926e0bda1f92fdee87c69181117cd006e5e8f09
곽민지 <mini0006@naver.com>
2023-12-14 19:03:32 +0900

    Implement Queue using Stacks

---
+++ 2023-12-14/leetcode_232.js
@@ -0,0 +1,20 @@
+class MyQueue {
+  constructor() {
+    this.stack = [];
+  }
+  push = (x) => {
+    this.stack.push(x);
+  };
+
+  pop = () => {
+    return this.stack.shift();
+  };
+
+  peek = () => {
+    return this.stack[0];
+  };
+
+  empty = () => {
+    return !this.stack.length;
+  };
+}
