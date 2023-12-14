e259cdaac98999061300da97242387e127cf1034
곽민지 <mini0006@naver.com>
2023-12-14 19:11:54 +0900

    Implement Stack using Queues

---
+++ 2023-12-14/leetcode_225.js
@@ -0,0 +1,24 @@
+class MyStack {
+  constructor() {
+    this.queue = [];
+  }
+
+  push = (x) => {
+    this.queue.push(x);
+    for (let i = 0; i < this.queue.length - 1; i++) {
+      this.queue.push(this.queue.shift());
+    }
+  };
+
+  pop = () => {
+    return this.queue.shift();
+  };
+
+  top = () => {
+    return this.queue[0];
+  };
+
+  empty = () => {
+    return this.queue.length === 0;
+  };
+}
--- 2023-12-14/leetcode_225.py
+++ 2023-12-14/leetcode_225.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 class MyStack(object):
-
     def __init__(self):
         self.queue = deque()
 
