3c19a38c612a70e38c16e427f3dc3d067e244bc7
곽민지 <mini0006@naver.com>
2023-12-16 02:12:07 +0900

    Top K Frequent Elements

--- 2023-12-15/leetcode_347.py
+++ 2023-12-15/leetcode_347.py
@@ -1,3 +1,4 @@
+#  counter를 생각하지 못해서 유감인 풀이
 class Solution:
     def topKFrequent(self, nums, k):
         numDict = dict()
@@ -8,3 +9,19 @@ class Solution:
         for i in range(k):
             answer.append(list(sortedDict.keys())[i])
         return answer;
+
+
+# counter를 사용한 풀이
+import collections
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        stack = []
+
+        for topNum, numCnt in collections.Counter(nums).most_common():
+            if not k:
+                break
+            stack.append(topNum)
+            k -= 1
+        return stack
+
