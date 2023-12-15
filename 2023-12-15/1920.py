bb0f779dbd4a3c591a08f2b22dbc8243ffc188df
곽민지 <mini0006@naver.com>
2023-12-15 15:12:41 +0900

    수 찾기

--- 2023-12-15/1920.py
+++ 2023-12-15/1920.py
@@ -1,2 +1,2 @@
 n, checkSet, m = input(), set(input().split()), input()
-answer = print('\n'.join(['1' if x in checkSet else '0' for x in input().split()]))
+print('\n'.join(['1' if x in checkSet else '0' for x in input().split()]))
