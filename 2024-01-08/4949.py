a7b1f35f62af9097263d1964df48d75abeea6c7a
곽민지 <mini0006@naver.com>
2024-01-09 09:57:54 +0900

    균형잡힌 세상

--- 2024-01-08/4949.py
+++ 2024-01-08/4949.py
@@ -1,20 +1,22 @@
 while True:
     s = input()
-    if s == '.':
+    if s == '.': # 입력종료 문자
         break
-    bracketStack = []
-    isBalance = True
+
+    bracketStack = [] # 여는 괄호들을 담아줄 스택
+    isBalance = True # VPS 여부
 
     for curr in s:
-        if curr == '(' or curr == '[':
+        if curr == '(' or curr == '[': # 여는 괄호가 나왔다면 스택에 쌓아준다.
             bracketStack.append(curr)
-        elif curr == ')' or  curr == ']':
+        elif curr == ')' or  curr == ']': # 닫힌 괄호가 나온 경우
+            # 열린 괄호가 있다면 마지막 열린 괄호를 저장
             poppedBracket = bracketStack.pop() if len(bracketStack) > 0 else ''
 
-            if (curr == ')' and poppedBracket != '(') or (curr == ']' and poppedBracket != '['):
+            if (curr == ')' and poppedBracket != '(') or (curr == ']' and poppedBracket != '['): # 서로 짝이 맞지 않는 경우 VPS가 아니다.
                 print('no')
                 isBalance = False
                 break
     
-    if isBalance:
+    if isBalance: # 남아있는 열린 괄호가 없을 때 VPS가 성립된다.
         print('yes' if not len(bracketStack) else 'no')
