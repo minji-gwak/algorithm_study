234279b5b9701b8ac46939c120eb7d8d3a861269
곽민지 <mini0006@naver.com>
2024-02-05 15:18:57 +0900

    트리

--- baekjoon/1068.js
+++ baekjoon/1068.js
@@ -2,19 +2,18 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const [L, data, K] = fs.readFileSync(filePath).toString().trim().split('\n');
 
+const dfs = (cutNum, list) => {
+  list[cutNum] = null;
+
+  for (let i = 0; i < list.length; i++) {
+    if (cutNum === list[i]) {
+      dfs(i, list);
+    }
+  }
+};
+
 const solution = (parentList, k) => {
   let cnt = 0;
-
-  const dfs = (cutNum, list) => {
-    list[cutNum] = null;
-
-    for (let i = 0; i < list.length; i++) {
-      if (cutNum === list[i]) {
-        dfs(i, list);
-      }
-    }
-  };
-
   dfs(k, parentList);
 
   for (let i = 0; i < parentList.length; i++) {
