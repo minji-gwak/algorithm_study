ad6f0e229ef2e5d60b6678ca215c4a5c099f177c
곽민지 <mini0006@naver.com>
2024-10-12 23:11:28 +0900

    Divide Intervals Into Minimum Number of Groups

--- leetcode/leetcode_2406.js
+++ leetcode/leetcode_2406.js
@@ -1,17 +1,17 @@
 const minGroups = (intervals) => {
     const points = [];
     intervals.forEach(([left, right]) => {
-        points.push([left, 'left']);
-        points.push([right, 'right']);
+        points.push([left, 1]);
+        points.push([right, -1]);
     });
 
-    points.sort((a, b) => (a[0] === b[0] ? a[1].localeCompare(b[1]) : a[0] - b[0]));
+    points.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
 
     let currCnt = 0;
     let maxCnt = 0;
 
     points.forEach(([num, type]) => {
-        if (type === 'left') {
+        if (type === 1) {
             currCnt++;
             maxCnt = Math.max(maxCnt, currCnt);
         } else {
