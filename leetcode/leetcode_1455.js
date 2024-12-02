66653be25e512a37f11e97395d4054a1ce26fd5e
Minji Gwak <mini0006@naver.com>
2024-12-03 01:14:26 +0900

    Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ leetcode/leetcode_1455.js
@@ -0,0 +1,10 @@
+const isPrefixOfWord = (sentence, searchWord) => {
+    const words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].startsWith(searchWord)) {
+            return i + 1;
+        }
+    }
+    return -1;
+};
